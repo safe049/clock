d3d8b5e1c830a0d5cb3bd3d07dc413a1d19a1377
safe049 <1329261154@qq.com>
2024-11-10 16:05:58 +0800

    Clock for ya ⌚

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# clock/__init__.py
+from .clock import get_time
---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='clock',
+    version='0.1',
+    packages=find_packages(),
+    description='Simple terminal clock',
+    author='dynamo',
+    author_email='1329261154@qq.com',
+    url='https://github.com/safe049/clock',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
